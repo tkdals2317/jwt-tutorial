9ba47c71f3a606d91d38b19fa618d48664b0f9d3
lsm0506 <lsm0506@midasin.com>
2022-12-02 15:07:44 +0900

    Initial commit

---
+++ src/main/java/com/sangmin/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.sangmin.jwttutorial.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근 제한 설정
+                .antMatchers("/api/hello").permitAll() // /api/hello 요청은 허용
+                .anyRequest().authenticated(); // 나머지는 인증을 받아야함
+    }
+}
